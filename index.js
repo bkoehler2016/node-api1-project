d52f79d3b5ec77cc145449b8d8a45a59cdd86db4
Ben Koehler <bkoehler85282@gmail.com>
2020-03-09 15:08:29 -0700

    changed server started message to link correctly

--- index.js
+++ index.js
@@ -110,5 +110,5 @@ server.put("/api/users/:id", (req, res) => {
 });
 const port = 5000;
 server.listen(port, () =>
-  console.log(` Server started on http://localhost/${port} `)
+  console.log(` Server started on http://localhost:${port} `)
 );
