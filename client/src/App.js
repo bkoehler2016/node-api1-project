c0de82421cdfebe7178111bfab21ddc8afcae373
Ben Koehler <bkoehler85282@gmail.com>
2020-03-09 15:02:18 -0700

    changed heading

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Users from "./components/Users";
 function App() {
   return (
     <div className="App">
-      <h2>Users</h2>
+      <h2>Lord of the Rings Characthers.</h2>
       <Users />
     </div>
   );
