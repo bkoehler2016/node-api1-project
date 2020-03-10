972073859a829447a80f86965b28376b14fcb695
Ben Koehler <bkoehler85282@gmail.com>
2020-03-09 18:55:44 -0700

    added styling

---
+++ client/src/components/UsersForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class UsersForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div></div>;
+  }
+}
+
+UsersForm.propTypes = {};
+
+export default UsersForm;
